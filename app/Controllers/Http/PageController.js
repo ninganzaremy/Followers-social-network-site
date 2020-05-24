382b2f251bf0504d717498b5948d7e9d52bd6cce
Ningaanza Remy <remimaradona@gmail.com>
2020-05-24 00:09:59 -0400

    check if a user logged in and show him home page, if not show the welcome page

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -3,7 +3,12 @@
 class PageController {
   async home({response, request, view, auth}){
 
-    return view.render('pages/react')
+    if (auth.user) {
+      return view.render('pages/react')
+    }else{
+
+      return view.render('pages/welcome')
+    }
   }
 
   async welcome({response, request, view}){
