d04052b74f01c6a98c3aea108de4c10bd64d7dd9
Ningaanza Remy <remimaradona@gmail.com>
2020-05-27 15:54:56 -0400

    track followers using user_id and follow_id from followers table in database

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,18 +1,67 @@
-'use strict'
-const User = use('App/Models/User')
+"use strict";
+const Database = use("Database");
+const User = use("App/Models/User");
 class UserController {
-  async profile({auth , request, response}){
+  async profile({ auth, request, response }) {
     try {
+      const user = await User.query()
+        .where("id", "=", request.params.id)
+        .fetch();
+      const following = await Database.from("followers").where({
+        user_id: 1,
+        follower_id: request.params.id,
+      });
 
-      const user = await User.query().where('id', '=', request.params.id).fetch()
-      console.log(user.toJSON());
+      console.log(following);
 
-      return user.toJSON()
+      return {
+        user: user.toJSON(),
+        following:
+          following === undefined || following.length == 0 ? false : true,
+      };
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+  }
+  async follow({ auth, request, response }) {
+    if (auth.user) {
+      try {
+        const followedUser = await Database.table("followers").insert({
+          user_id: auth.user.id,
+          follower_id: request.params.id,
+        });
+        console.log(followedUser);
+        return "saved successfully";
 
+        return user.toJSON();
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      return "you are not logged in";
+    }
+  }
+
+  async unfollow({ auth, request, response }) {
+    if (auth.user) {
+      try {
+        const followedUser = await Database.table("followers")
+          .where({
+            user_id: auth.user.id,
+            follower_id: request.params.id,
+          })
+          .delete();
+        console.log(followedUser);
+        return "deleted successfully";
+
+        return user.toJSON();
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      return "you are not logged in";
+    }
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
